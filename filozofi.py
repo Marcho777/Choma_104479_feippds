d7d74237b6464c7e7fad19f5945b09db22325f2e
Marcho777 <m.choma07@gmail.com>
2023-03-12 17:24:41 +0100

    style: pridanie docstringov

--- filozofi.py
+++ filozofi.py
@@ -1,69 +1,84 @@
-"""
+""" Tento modul implementuje problém večerujúcich filozofov.
+
+Implemetácia využíva riešenie pomocou ľavákov a pravákov.
 """
 
-__authors__ = "Marián Choma"
-__email__ = "xchoma@stuba.sk"
+__authors__ = "Marián Choma, Tomáš Vavro"
+__email__ = "xchoma@stuba.sk, xvavro@stuba.sk"
 __license__ = "MIT"
 
-from fei.ppds import Mutex, Thread, Semaphore, print
+from fei.ppds import Thread, Semaphore, print
 from time import sleep
-from random import uniform
 
 POC_FILOZOFOV = 5
 POC_KOL = 10
 
 
 class Zdielane:
-    """Represent shared data for all threads."""
+    """Trieda reprezentuje zdieľané dáta pre všetky vlákna"""
+
     def __init__(self):
-        """Initialize an instance of Shared."""
-        self.vydlicky = [Semaphore(1) for _ in range(POC_FILOZOFOV)]
+        """Inicializácia parametru vidličky, ktoré reprezentujú semafory"""
+        self.vidlicky = [Semaphore(1) for _ in range(POC_FILOZOFOV)]
 
 
-def rozmyslanie(i):
+def rozmyslanie(i: int):
+    """Simuluje čas na premýšľanie.
+
+    :param i: Id vlákna
+    :return:
+    """
     print(f"Filozof {i} rozmýšľa")
     sleep(0.1)
 
 
-def veceranie(i):
+def veceranie(i: int):
+    """Simuluje čas na večeranie
+
+    :param i: Id vlákna
+    :return:
+    """
     print(f"Filozof {i} večeria")
     sleep(0.2)
 
 
 def filozof(i: int, zdielane: Zdielane):
-    """Run philosopher's code.
-    Args:
-        i -- philosopher's id
-        shared -- shared data
+    """Reprezentuje správanie filozofa
+
+    :param i: Id vlákna
+    :param zdielane: objekt triedy zdielane
+    :return:
     """
     for _ in range(POC_KOL):
         rozmyslanie(i)
-        # get forks
         left_fork = i
         right_fork = (i + 1) % POC_FILOZOFOV
         if i % 2 == 0:
-            zdielane.vydlicky[left_fork].wait()
-            print(f"Filozof {i} si vzal lavu vydlicku a caka na pravu")
-            zdielane.vydlicky[right_fork].wait()
-            print(f"Filozof {i} si vzal pravu vydlicku a moze jest")
+            zdielane.vidlicky[right_fork].wait()
+            print(f"Filozof {i} si vzal pravu vydlicku a caka na lavu.")
+            zdielane.vidlicky[left_fork].wait()
+            print(f"Filozof {i} si vzal lavu vydlicku a zacal jest")
         else:
-            zdielane.vydlicky[right_fork].wait()
-            print(f"Filozof {i} si vzal pravu vydlicku a caka na pravu")
-            zdielane.vydlicky[left_fork].wait()
-            print(f"Filozof {i} si vzal lavu vydlicku a moze jest")
+            zdielane.vidlicky[left_fork].wait()
+            print(f"Filozof {i} si vzal lavu vydlicku a caka na pravu")
+            zdielane.vidlicky[right_fork].wait()
+            print(f"Filozof {i} si vzal pravu vydlicku a moze zacat jest")
+
         veceranie(i)
-        # put forks back
-        zdielane.vydlicky[left_fork].signal()
+
+        zdielane.vidlicky[left_fork].signal()
         print(f"Filozof {i} vratil lavu vidlicku")
-        zdielane.vydlicky[right_fork].signal()
+        zdielane.vidlicky[right_fork].signal()
         print(f"Filozof {i} vratil pravu vidlicku")
 
+
 def main():
-    """Run main."""
+    """Vytvorenie vlákien a objektu triedy Zdielane.
+
+    :return:
+    """
     zdielane: Zdielane = Zdielane()
-    filozofi: list[Thread] = [
-        Thread(filozof, i, zdielane) for i in range(POC_FILOZOFOV)
-    ]
+    filozofi: list[Thread] = [Thread(filozof, i, zdielane) for i in range(POC_FILOZOFOV)]
     for p in filozofi:
         p.join()
 
